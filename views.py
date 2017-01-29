d098ad462a4d10ce82dc1f162ae89e4e97f1ac90
Peter S. C <PSamuelC@users.noreply.github.com>
2017-01-29 00:06:50 +0000

    added some css

--- views.py
+++ views.py
@@ -1,12 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
-#app.config.from_object('config') # read our config.py file
-#from app import app
 from flask import render_template, redirect, request
-#from forms import SearchForm
+
+import traceback
+import logging
 
 @app.route('/', methods=['GET'])
-#@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET'])
 def show_form():
     '''Shows the homepage, with a search form'''
     return render_template('indextemplate.html')
@@ -14,24 +14,32 @@ def show_form():
 @app.route('/results', methods=['POST', 'GET'])
 def show_results():
     '''Calls the backend functions to obtain data, which is then shown'''
-    print "called"
-    print request.form
-    start = [float(request.form['lat_a']),
-            float(request.form['long_a'])]
-    end = [float(request.form['lat_b']),
-            float(request.form['long_b'])]
+    try:
+        start = [float(request.form['lat_a']),
+                float(request.form['long_a'])]
+        end = [float(request.form['lat_b']),
+                float(request.form['long_b'])]
+                
+    except Exception as e:
+        print "Error caught:\n",e
+        return redirect('/error')
     
-    delta_h = elevation(start, end)
-    dist = bike_dist(start, end)
-    nearest_lock = bike_park_dist(end)
-    #plus a bunch more cool things
-    
-    return render_template('resultstemplate.html',
-                            searched=request.form,
-                            delta_h=delta_h,
-                            dist=dist,
-                            nearest_lock=nearest_lock) #etc
-    
+    else:
+        delta_h = elevation(start, end)
+        dist = bike_dist(start, end)
+        nearest_lock = bike_park_dist(end)
+        #plus a bunch more cool things
+        
+        return render_template('resultstemplate.html',
+                                searched=request.form,
+                                delta_h=delta_h,
+                                dist=dist,
+                                nearest_lock=nearest_lock) #etc
+
+@app.route('/error')
+def show_error():
+    return render_template('errortemplate.html')
+
 # --------------------------------------------------
 # placeholder functions
 # these will be deleted once the functions to import are written
